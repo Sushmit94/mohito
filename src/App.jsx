0563e5fc9928a4d981981d93fe42a6a8b502425a
unknown <sushmitshrm@gmail.com>
2025-07-10 15:33:22 +0530

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import Herot from './components/Herot';
 import Cocktails from './components/Cocktails';
 import About from './components/About';
 import Art from './components/Art';
+import Menu from './components/Menu';
+import Contact from './components/Contact';
+
 import React from 'react';  
 const App  = () => {
   return (
@@ -17,6 +20,8 @@ const App  = () => {
       <Cocktails />
       <About />
       <Art/>
+      <Menu/>
+      <Contact />
     </main>
   )
 }
